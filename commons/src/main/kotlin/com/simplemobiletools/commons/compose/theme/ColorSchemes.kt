0c5652d7e18f9af43c5f0bf332f27a75c7e572ff
Ensar Sarajčić <dev@ensarsarajcic.com>
2023-10-02 10:27:51 +0200

    Add contrast colors to color schemes

--- commons/src/main/kotlin/com/simplemobiletools/commons/compose/theme/ColorSchemes.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/compose/theme/ColorSchemes.kt
@@ -2,14 +2,23 @@ package com.simplemobiletools.commons.compose.theme
 
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import com.simplemobiletools.commons.extensions.getContrastColor
 
 internal val darkColorScheme = darkColorScheme(
     primary = color_primary,
+    onPrimary = Color(color_primary.toArgb().getContrastColor()),
     secondary = color_primary_dark,
+    onSecondary = Color(color_primary_dark.toArgb().getContrastColor()),
     tertiary = color_accent,
+    onTertiary = Color(color_accent.toArgb().getContrastColor()),
 )
 internal val lightColorScheme = lightColorScheme(
     primary = color_primary,
+    onPrimary = Color(color_primary.toArgb().getContrastColor()),
     secondary = color_primary_dark,
+    onSecondary = Color(color_primary_dark.toArgb().getContrastColor()),
     tertiary = color_accent,
+    onTertiary = Color(color_accent.toArgb().getContrastColor()),
 )
